a85115a3b0dd3dc658fe7900073ea94d32fa8bce
mkasyanov <mkasyanov@greet-go.com>
2019-05-15 19:15:35 +0600

    add xref component

---
+++ mobile/src/app/dynamic-form/components/form_elements/xref/xref.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-xref',
+  templateUrl: './xref.component.html',
+  styleUrls: ['./xref.component.scss'],
+})
+export class XrefComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
